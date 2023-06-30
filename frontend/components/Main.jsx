1adc44100375ceb6d87d40943c72eec05374617b
Bastos974 <sebastien.lemoine974@gmail.com>
2023-06-30 19:27:36 +0400

    display the tabs for authorized addresses

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -3,9 +3,28 @@ import { useState, useEffect } from "react";
 import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
 import Admin from "./Admin/Admin";
 import Voters from "./Voter/Voters";
+import {  useContractRead, useAccount } from "wagmi";
+import Contract from "../public/Voting.json";
+
+
 
 function Main() {
    const [mounted, setMounted] = useState(false);
+   const { address: addrAccount } = useAccount();
+
+
+
+   const { data: owner } = useContractRead({
+      address: process.env.NEXT_PUBLIC_CONTRACT_ADDRESS,
+      abi: Contract.abi,
+      functionName: "owner",
+      account: addrAccount,
+      onError(error) {
+        console.log("Error", error);
+      },
+    });
+
+
 
    useEffect(() => {
       setMounted(true);
@@ -16,19 +35,47 @@ function Main() {
       <div>
          <Tabs variant="enclosed">
             <TabList>
+
+
+            {  addrAccount === owner && (
                <Tab>Admin</Tab>
+            )}
+
+
+               {  addrAccount !== owner && (
                <Tab>Voters</Tab>
+
+               )}
             </TabList>
+
             <TabPanels>
+
+         
+              
+            
+               
+               
                <TabPanel>
                   <Admin />
+
                </TabPanel>
+          
+
+            
+
                <TabPanel>
                   <Voters />
                </TabPanel>
+             
+
+
+
+
+
+
             </TabPanels>
          </Tabs>
-         ;
+         
       </div>
    );
 }
