ffa9286172e7ea94fa23fe728f1dd1bbf75098bc
Bastos974 <sebastien.lemoine974@gmail.com>
2023-07-01 00:31:21 +0400

    corection2 script for navigate throw in app

--- backend/scripts/thescript/allStep.js
+++ backend/scripts/thescript/allStep.js
@@ -2,10 +2,6 @@
 // lancer le script
 //http://localhost:3000
 
-
-//////////////////
-
-
 const { ethers } = require("hardhat");
 
 function wait(delay) {
@@ -24,59 +20,59 @@ async function main() {
 
   const add1 = await contract.addVoter(account1.address);
   console.log("Résultat de l'appel de fonction :", add1);
+  await wait(4000);
 
   const add2 = await contract.addVoter(account2.address);
   console.log("Résultat de l'appel de fonction :", add2);
+  await wait(4000);
 
   const add3 = await contract.addVoter(account3.address);
   console.log("Résultat de l'appel de fonction :", add3);
+  await wait(4000);
 
   const add4 = await contract.addVoter(account4.address);
   console.log("Résultat de l'appel de fonction :", add4);
+  await wait(4000);
 
   const add5 = await contract.addVoter(account5.address);
   console.log("Résultat de l'appel de fonction :", add5);
-
   await wait(4000);
+
   console.log("4 secondes se sont écoulées !");
 
   const startProposal = await contract.startProposalsRegistering();
   console.log("Résultat de l'appel de fonction :", startProposal);
-
   await wait(4000);
-  console.log("4 secondes se sont écoulées !");
 
   const proposal1 = await contract.connect(account1).addProposal("Rouge");
   console.log("Résultat de l'appel de fonction :", proposal1);
+  await wait(4000);
 
   const proposal2 = await contract.connect(account2).addProposal("Vert");
   console.log("Résultat de l'appel de fonction :", proposal2);
+  await wait(4000);
 
   const proposal3 = await contract.connect(account3).addProposal("Jaune");
   console.log("Résultat de l'appel de fonction :", proposal3);
+  await wait(4000);
 
   const proposal4 = await contract.connect(account4).addProposal("bleu");
   console.log("Résultat de l'appel de fonction :", proposal4);
+  await wait(4000);
 
   const proposal5 = await contract.connect(account5).addProposal("noir");
   console.log("Résultat de l'appel de fonction :", proposal5);
-
   await wait(4000);
+
   console.log("4 secondes se sont écoulées !");
 
   const endProposal = await contract.endProposalsRegistering();
   console.log("Résultat de l'appel de fonction :", endProposal);
-
   await wait(4000);
-  console.log("4 secondes se sont écoulées !");
 
   const startVoting = await contract.startVotingSession();
   console.log("Résultat de l'appel de fonction :", startVoting);
-
-  const waitFourSecondsC = async () => {
-    await wait(4000);
-    console.log("4 secondes se sont écoulées !");
-  };
+  await wait(4000);
 
   const votedPoposal1 = await contract.connect(account1).setVote(1);
   console.log("Résultat de l'appel de fonction :", votedPoposal1);
@@ -84,24 +80,20 @@ async function main() {
   const votedProposal2 = await contract.connect(account2).setVote(2);
   console.log("Résultat de l'appel de fonction :", votedProposal2);
 
-  const votedPproposal3 = await contract.connect(account3).setVote(2);
-  console.log("Résultat de l'appel de fonction :", votedPproposal3);
+  const votedProposal3 = await contract.connect(account3).setVote(2);
+  console.log("Résultat de l'appel de fonction :", votedProposal3);
 
   const votedProposal4 = await contract.connect(account4).setVote(2);
   console.log("Résultat de l'appel de fonction :", votedProposal4);
 
-  const votedPproposal5 = await contract.connect(account5).setVote(3);
-  console.log("Résultat de l'appel de fonction :", votedPproposal5);
+  const votedProposal5 = await contract.connect(account5).setVote(3);
+  console.log("Résultat de l'appel de fonction :", votedProposal5);
 
-  await waitFourSecondsC();
+  console.log("4 secondes se sont écoulées !");
 
   const endVoting = await contract.endVotingSession();
   console.log("Résultat de l'appel de fonction :", endVoting);
-
-  const waitFourSecondsD = async () => {
-    await wait(4000);
-    console.log("4 secondes se sont écoulées !");
-  };
+  await wait(4000);
 
   const tallyVotes = await contract.tallyVotes();
   console.log("Résultat de l'appel de fonction :", tallyVotes);
@@ -113,3 +105,10 @@ main()
     console.error(error);
     process.exit(1);
   });
+
+
+
+
+
+
+
