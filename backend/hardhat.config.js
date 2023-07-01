e7ec6f549de0a16df0587e248988d818b332aa96
Hafid <hafid.saou@gmail.com>
2023-07-02 00:22:40 +0200

    add .env.example, and verify command

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,19 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
    solidity: "0.8.13",
+   defaultNetwork: "localhost",
+   networks: {
+      sepolia: {
+         url: "https://sepolia.infura.io/v3/" + process.env.INFURA_KEY,
+         accounts: [process.env.DEPLOYER_PRIVATE_KEY],
+      },
+   },
+   etherscan: {
+      apiKey: {
+         sepolia: process.env.ETHERSCAN_API_KEY,
+      },
+   },
 };
