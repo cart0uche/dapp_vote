8b76bdc3e21236162d7e0d1fb4f3606cec451f5b
Hafid <hafid.saou@gmail.com>
2023-07-03 15:25:27 +0200

    add role management with "voter"

--- frontend/components/Main.jsx
+++ frontend/components/Main.jsx
@@ -1,13 +1,26 @@
 "use client";
 import { useState, useEffect } from "react";
-import { Tabs, TabList, TabPanels, Tab, TabPanel } from "@chakra-ui/react";
+import {
+   Tabs,
+   TabList,
+   TabPanels,
+   Tab,
+   TabPanel,
+   Text,
+   Box,
+} from "@chakra-ui/react";
 import Admin from "./Admin/Admin";
 import Voters from "./Voter/Voters";
 import { useContractRead, useAccount } from "wagmi";
 import Contract from "../public/Voting.json";
+import { useVoteContext } from "@/components/voteContext";
+import { fetchVoters } from "./fetchData.jsx";
 
 function Main() {
+   const { newVoter } = useVoteContext();
    const [mounted, setMounted] = useState(false);
+   const [voters, setVoters] = useState([]);
+   const [isVoter, setIsVoter] = useState(false);
    const { address: addrAccount } = useAccount();
 
    const { data: owner } = useContractRead({
@@ -23,6 +36,26 @@ function Main() {
    useEffect(() => {
       setMounted(true);
    }, []);
+
+   // un peu tricky : dès qu'un nouveau voter est ajouté on recupere ici la liste des voters pour vérifier si le
+   // compte courant s'y trouve (seulement si on est pas déjà un votant)
+   useEffect(() => {
+      const _fetchVoters = async () => {
+         if (isVoter === false) {
+            await fetchVoters(setVoters);
+         }
+      };
+      _fetchVoters();
+   }, [newVoter]);
+
+   useEffect(() => {
+      voters.forEach((voter) => {
+         if (voter.address === addrAccount) {
+            setIsVoter(true);
+         }
+      });
+   }, [voters]);
+
    if (!mounted) return <></>;
 
    return (
@@ -39,9 +72,21 @@ function Main() {
                      <Admin />
                   </TabPanel>
                ) : null}
-               <TabPanel>
-                  <Voters />
-               </TabPanel>
+               {isVoter ? (
+                  <TabPanel>
+                     <Voters />
+                  </TabPanel>
+               ) : (
+                  <Box
+                     display="flex"
+                     justifyContent="center"
+                     alignItems="center"
+                  >
+                     <Text fontSize="xl" marginTop="2rem">
+                        Ask the admin to add you in the voters list
+                     </Text>
+                  </Box>
+               )}
             </TabPanels>
          </Tabs>
       </div>
